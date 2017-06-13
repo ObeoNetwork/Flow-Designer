9a33c0b6e4619da93a60cace8f7f48ef68808d55
cbrun <cedric.brun@obeo.fr>
2017-06-13 17:33:34 +0200

    Streamline renaming by delegating to FlowServices.editNameAndOptionallyCapacity(Named, String)

--- plugins/fr.obeo.dsl.designer.sample.flow.design/src/fr/obeo/dsl/designer/sample/flow/design/FlowServices.java
+++ plugins/fr.obeo.dsl.designer.sample.flow.design/src/fr/obeo/dsl/designer/sample/flow/design/FlowServices.java
@@ -1,9 +1,43 @@
 package fr.obeo.dsl.designer.sample.flow.design;
 
+import fr.obeo.dsl.designer.sample.flow.CapacityBound;
+import fr.obeo.dsl.designer.sample.flow.Named;
 import fr.obeo.dsl.designer.sample.flow.Processor;
 
 public class FlowServices {
 	public int sizeFromCapacity(Processor s) {
 		return Double.valueOf(Math.sqrt(s.getCapacity()) * 2).intValue();
+	}
+
+	public Named editNameAndOptionallyCapacity(Named n, String typedString) {
+		int indexSep = typedString.indexOf(':');
+		int capacity = 0;
+		if (n instanceof CapacityBound) {
+			capacity = ((CapacityBound) n).getCapacity();
+		}
+		String name = typedString;
+		if (indexSep != -1) {
+			name = typedString.substring(0, indexSep);
+			String capacityStr = typedString.substring(indexSep + 1).trim();
+			try {
+				capacity = Integer.valueOf(capacityStr);
+			} catch (NumberFormatException e) {
+				// ignore
+			}
+		}
+		if (n instanceof CapacityBound) {
+			((CapacityBound) n).setCapacity(capacity);
+		}
+
+		if (name != null && name.length() > 0) {
+			name = name.replace(' ', '_');
+			if (!name.equals(n.getName())) {
+				/*
+				 * TODO refactor String attributes?
+				 */
+			}
+			n.setName(name);
+		}
+		return n;
 	}
 }
