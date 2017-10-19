3badc3b8e3f6447cb4a8ad694e3c41e9be218ced
cbrun <cedric.brun@obeo.fr>
2017-10-19 16:27:13 +0200

    Make Flow use XMI-IDs

---
+++ plugins/fr.obeo.dsl.designer.sample.flow/src-gen/fr/obeo/dsl/designer/sample/flow/util/FlowResourceImpl.java
@@ -0,0 +1,33 @@
+/**
+ */
+package fr.obeo.dsl.designer.sample.flow.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see fr.obeo.dsl.designer.sample.flow.util.FlowResourceFactoryImpl
+ * @generated
+ */
+public class FlowResourceImpl extends XMIResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public FlowResourceImpl(URI uri) {
+		super(uri);
+	}
+	
+	@Override
+	protected boolean useUUIDs() {
+		return true;
+	}
+
+} //FlowResourceImpl
